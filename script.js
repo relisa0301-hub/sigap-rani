e563527baaf89922ab11751d22428f6363cddfb9
relisa0301-hub <relisa0301@gmail.com>
2026-07-01 13:37:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
+// ================================
+// SIGAP RANI
+// Digital Clock
+// ================================
+
+function updateClock(){
+
+const now = new Date();
+
+const hari=[
+"Minggu",
+"Senin",
+"Selasa",
+"Rabu",
+"Kamis",
+"Jumat",
+"Sabtu"
+];
+
+const bulan=[
+"Januari",
+"Februari",
+"Maret",
+"April",
+"Mei",
+"Juni",
+"Juli",
+"Agustus",
+"September",
+"Oktober",
+"November",
+"Desember"
+];
+
+let jam=String(now.getHours()).padStart(2,"0");
+let menit=String(now.getMinutes()).padStart(2,"0");
+let detik=String(now.getSeconds()).padStart(2,"0");
+
+document.getElementById("jam").innerHTML=
+`${jam} : ${menit} : ${detik}`;
+
+const tanggal=
+`${hari[now.getDay()]}, ${now.getDate()} ${bulan[now.getMonth()]} ${now.getFullYear()}`;
+
+const elTanggal=document.getElementById("tanggal");
+
+if(elTanggal){
+
+elTanggal.innerHTML=tanggal;
+
+}
+
+}
+
+setInterval(updateClock,1000);
+
+updateClock();
+
 console.log("SIGAP RANI aktif");
