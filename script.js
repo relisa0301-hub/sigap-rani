bd27eff9deee7d0c1eaf265490b4d8fd1359b733
relisa0301-hub <relisa0301@gmail.com>
2026-07-01 12:16:12 +0700

    Update script.js
    baru

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-function scrollToSection(id) {
-  document.getElementById(id).scrollIntoView({ behavior: "smooth" });
-}
-
-document.getElementById("menuBtn").addEventListener("click", function () {
-  const nav = document.getElementById("nav");
-
-  if (nav.style.display === "flex") {
-    nav.style.display = "none";
-  } else {
-    nav.style.display = "flex";
-    nav.style.flexDirection = "column";
-  }
-});
+console.log("SIGAP RANI aktif");
