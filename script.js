c5b8082103ebc7c2c19884cdbd4de11e2a875c99
relisa0301-hub <relisa0301@gmail.com>
2026-07-01 11:14:22 +0700

    Create script.js
    Menambahkan fungsi tombol Masuk Aplikasi

---
+++ script.js
@@ -0,0 +1,3 @@
+document.querySelector(".btn").addEventListener("click", function () {
+    window.location.href = "https://www.appsheet.com/start/a0bb851c-dfd7-4280-96a1-ba27da2ea92d?platform=desktop#appName=DATASISWASMARTATTENDQR-640092988&vss=H4sIAAAAAAAAA6XOsQ7CIBgE4He5mSdgq9rBGDuo6SIOWP4mxBaaQtWG8O5SrXFuHDny3X8Bd02Po5fVDfwcfq8djeAIAqexIwEusLbG97YRYAKFbD_hKj9kxSYTiIgX9uWeHHhYpPlftxm0IuN1ramfqiaYKmaWvieUgpkgMrSDl9eG3msTiTFlta0GR6pMQ5YPcFuTPztp1N6q1FjLxlF8AdQ61lxgAQAA&view=BERANDA";
+});
