4d1c7abf664e83b26a748a17baf677440ef6c5ab
relisa0301-hub <relisa0301@gmail.com>
2026-07-01 12:05:57 +0700

    Update script.js
    perbaikan

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-document.querySelector(".btn").addEventListener("click", function () {
-    window.location.href = "https://www.appsheet.com/start/a0bb851c-dfd7-4280-96a1-ba27da2ea92d?platform=desktop#appName=DATASISWASMARTATTENDQR-640092988&vss=H4sIAAAAAAAAA6XOsQ7CIBgE4He5mSdgq9rBGDuo6SIOWP4mxBaaQtWG8O5SrXFuHDny3X8Bd02Po5fVDfwcfq8djeAIAqexIwEusLbG97YRYAKFbD_hKj9kxSYTiIgX9uWeHHhYpPlftxm0IuN1ramfqiaYKmaWvieUgpkgMrSDl9eG3msTiTFlta0GR6pMQ5YPcFuTPztp1N6q1FjLxlF8AdQ61lxgAQAA&view=BERANDA";
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+}
+
+// tombol menu sederhana (mobile)
+document.getElementById("menuBtn").addEventListener("click", function () {
+  const nav = document.querySelector("nav");
+  if (nav.style.display === "block") {
+    nav.style.display = "none";
+  } else {
+    nav.style.display = "block";
+  }
 });
