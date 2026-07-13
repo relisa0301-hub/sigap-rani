74313b40bc3076e2f182d0ad3b196c9a9f216bad
relisa0301-hub <relisa0301@gmail.com>
2026-07-13 17:10:26 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,41 @@
+// ==========================================
+// SIGAP RANI V2
+// FIREBASE CONFIG
+// ==========================================
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
+import {
+getFirestore
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+import {
+getAuth
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+const firebaseConfig = {
+
+apiKey: "AIzaSyDLHdalkrTj_VMI7TPf2its4eE-89HodTY",
+
+authDomain: "sigap-rani.firebaseapp.com",
+
+projectId: "sigap-rani",
+
+storageBucket: "sigap-rani.firebasestorage.app",
+
+messagingSenderId: "25822414711",
+
+appId: "1:25822414711:web:d564982c21fad3bc310372"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+const auth = getAuth(app);
+
+window.db = db;
+window.auth = auth;
+
+console.log("SIGAP RANI Firebase Connected");
