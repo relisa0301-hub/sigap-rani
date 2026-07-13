2257372d8ca6660d87e76fae115c0ecd8d9f110e
relisa0301-hub <relisa0301@gmail.com>
2026-07-13 17:28:16 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,57 @@
+import { auth } from "./firebase.js";
 
+import {
+signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+const tombol = document.getElementById("loginBtn");
+
+const email = document.getElementById("email");
+
+const password = document.getElementById("password");
+
+const pesan = document.getElementById("pesan");
+
+tombol.addEventListener("click", () => {
+
+    pesan.innerHTML = "Sedang login...";
+
+    signInWithEmailAndPassword(
+
+        auth,
+
+        email.value.trim(),
+
+        password.value.trim()
+
+    )
+
+    .then((userCredential)=>{
+
+        localStorage.setItem(
+
+            "login",
+
+            "true"
+
+        );
+
+        localStorage.setItem(
+
+            "email",
+
+            userCredential.user.email
+
+        );
+
+        window.location.href="dashboard.html";
+
+    })
+
+    .catch((error)=>{
+
+        pesan.innerHTML="❌ "+error.message;
+
+    });
+
+});
