0ce8c79b7f91722a0d165e2e9fb7eb1b29320d47
relisa0301-hub <relisa0301@gmail.com>
2026-07-14 16:16:47 +0700

    Update auth.js
    auth.js V1

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,38 @@
-// auth.js
+// =====================================
+// SIGAP RANI V3
+// js/auth.js
+// =====================================
+
+import { auth } from "../firebase.js";
+
+import {
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
+export async function login(email, password) {
+
+  return await signInWithEmailAndPassword(
+    auth,
+    email,
+    password
+  );
+
+}
+
+export async function logout() {
+
+  return await signOut(auth);
+
+}
+
+export function cekLogin(callback) {
+
+  onAuthStateChanged(auth, (user) => {
+
+    callback(user);
+
+  });
+
+}
